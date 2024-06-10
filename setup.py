56af5996d77addc38f190a944b2ea653fe4173c1
Abrar1250 <mohamedabrar028@gmail.com>
2024-06-10 20:30:39 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
+REPO_NAME = "Chicken-Disease-Classification"
 AUTHOR_USER_NAME = "Abrar"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "mohamedabrar028@gmail.com"
